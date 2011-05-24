55252405d9f56183e0ae51d08e4b5c9921d6a90f
armontoubman <armontoubman@942c0738-ba76-46ba-9eb3-c4bc32fd7008>
2011-05-24 20:16:21 +0000

    9 seconden

--- SudokuCSP/src/sudokucsp/Timer.java
+++ SudokuCSP/src/sudokucsp/Timer.java
@@ -44,6 +44,30 @@ class Timer {
         training = readSudokus("sudoku_training.txt");
         top95 = readSudokus("top95.txt");
         
+      /* gedaan
+        //
+        Solver.HIDDENSINGLES = true;
+        Solver.NAKEDPAIRS = true;
+        Solver.HIDDENPAIRS = true;
+        test("rv_hs_hp_np");
+        
+        //
+        Solver.HEURISTIC1 = true;
+        test("rv_hs_hp_np_h1");
+        
+        //
+        Solver.HEURISTIC1 = false;
+        Solver.HEURISTIC3 = true;
+        test("rv_hs_hp_np_h3");
+        
+        //
+        Solver.HEURISTIC3 = false;
+        Solver.HEURISTIC13 = true;
+        test("rv_hs_hp_np_h13");
+      */
+        // {top95_rv_hs_hp_np=9.685215524666667, top95_rv_hs_hp_np_h13=9.13048304, training_rv_hs_hp_np_h1=26.819459991, training_rv_hs_hp_np_h3=25.575965089666667, top95_rv_hs_hp_np_h1=9.618339162333333, top95_rv_hs_hp_np_h3=9.190885884666669, training_rv_hs_hp_np_h13=25.511620738333335, training_rv_hs_hp_np=26.050099973333335}
+        
+      /* gedaan
         //
         Solver.HIDDENSINGLES = true;
         test("rv_hs");
@@ -57,6 +81,25 @@ class Timer {
         Solver.NAKEDPAIRS = false;
         Solver.HIDDENPAIRS = true;
         test("rv_hp");
+        
+        //
+        Solver.HIDDENPAIRS = false;
+        Solver.HIDDENSINGLES = true;
+        Solver.NAKEDPAIRS = true;
+        test("rv_hs_np");
+        
+        //
+        Solver.NAKEDPAIRS = false;
+        Solver.HIDDENPAIRS = true;
+        test("rv_hs_hp");
+        
+        //
+        Solver.HIDDENSINGLES = false;
+        Solver.NAKEDPAIRS = true;
+        test("rv_hp_np");
+      */
+        //{top95_rv_hp_np=230.84322473, training_rv_hp=489.462033154, training_rv_hp_np=81.19639021366667, top95_rv_hp=1596.8818117199999, training_rv_hs_hp=27.667227801666666, training_rv_hs_np=23.296235251333332, top95_rv_hs_hp=15.692717729, top95_rv_hs_np=20.824697618666665, training_rv_np=76.90551903866667, training_rv_hs=29.234435128, top95_rv_np=1172.4098914453334, top95_rv_hs=47.415598335}
+        
     }
     
     ArrayList<String> readSudokus(String filename)
